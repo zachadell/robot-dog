539af71f8fd3e0d489a576f1661de72999cddf2e
zachadell <zachary.adell@gmail.com>
2025-12-21 21:49:14 -0600

    Update state.py
    add the LegState class

--- simulation/dynamics/state.py
+++ simulation/dynamics/state.py
@@ -1,4 +1,11 @@
-class JointState:
+class JointState: # just a single joint - angle & angular velocity
   def __init__(self, theta=0.0, omega=0.0):
     self.theta = theta
     self.omega = omega
+
+class LegState: # 2 link leg - angle & angular velocity of both links
+  def __init__(self, link1theta=0.0, link1omega=0.0, link1length=0.0, link2theta=0.0, link2omega=0.0, link2length=0.0):
+    self.joint1 = JointState(link1theta, link1omega)
+    self.length1 = link1length
+    self.joint2 = JointState(link2theta, link2omega)
+    self.length2 = link2length
