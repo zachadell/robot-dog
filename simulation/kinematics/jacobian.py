9b3a2b6891e214841def5d083057b07bffd26248
zachadell <zachary.adell@gmail.com>
2025-12-24 01:21:17 -0600

    Update jacobian.py

--- simulation/kinematics/jacobian.py
+++ simulation/kinematics/jacobian.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def numerical_jacobian(vars, func, d=1e-10):
+def numerical(vars, func, d=1e-10):
   J = np.zeros((2, len(vars)))
   f0 = func(vars)
   for i in range(len(vars)):
