2d934a46cc7522184c2a1b6b60888a5700c4e462
zachadell <zachary.adell@gmail.com>
2025-12-28 01:17:21 -0600

    Update forward_kinematics.py

--- simulation/kinematics/forward_kinematics.py
+++ simulation/kinematics/forward_kinematics.py
@@ -2,5 +2,5 @@ import numpy as np
 
 def foot_pos(legState):
   x = legState.length1 * np.cos(legState.joint1.theta) + legState.length2 * np.cos(legState.joint1.theta + legState.joint2.theta)
-  x = legState.length1 * np.sin(legState.joint1.theta) + legState.length2 * np.sin(legState.joint1.theta + legState.joint2.theta)
+  y = legState.length1 * np.sin(legState.joint1.theta) + legState.length2 * np.sin(legState.joint1.theta + legState.joint2.theta)
   return np.array([x, y])
